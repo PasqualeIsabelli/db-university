1160d633e29e6b22869c169fc791fa172bf50a00
PasqualeIsabelli <pasquale0395@gmail.com>
2023-09-14 16:50:43 +0200

    fixed

--- query.sql
+++ query.sql
@@ -16,14 +16,14 @@ WHERE YEAR(CURRENT_DATE) - YEAR(`date_of_birth`) > 30;
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT *
 FROM `courses`
-WHERE `period` = `I semestre`
+WHERE `period` = 'I semestre'
 	AND `year` = 1;
     
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-SELECT *
-FROM `exams`
-WHERE `date` = "2020-6-20"
-	AND `hour` = "14:00";
+SELECT * 
+FROM `exams` 
+WHERE `date` = "2020-06-20"
+	AND `hour` > "14:00";
     
 -- 6. Selezione tutti i corsi di laurea magistrale (38)
 SELECT *
